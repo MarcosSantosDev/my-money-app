a4a29fa7be9a1c988cb7c26493023f6cb97ed129
Marcos Santos <marcosadriano034@gmail.com>
2021-03-07 00:55:54 -0300

    fix: renamed BillingCycle model property 'require' to 'required'

--- backend/src/api/billingCycle/billingCycle.js
+++ backend/src/api/billingCycle/billingCycle.js
@@ -5,37 +5,37 @@ const mongoose = restful.mongoose
 const creditSchema = new mongoose.Schema({
     name: {
         type: String,
-        require: true
+        required: true
     },
     value: {
         type: Number,
         min: 0,
-        require: true
+        required: true
     }
 })
 
 const debtSchema = new mongoose.Schema({
     name: {
         type: String,
-        require: true
+        required: true
     },
     value: {
         type: Number,
         min: 0,
-        require: true
+        required: true
     },
     status: {
         type: String,
-        require: false,
+        required: false,
         uppercase: true,
         enum: ['PAGO', 'PENDENTE', 'AGENDADO']
     },
 })
 
 const billingCycleSchema = new mongoose.Schema({
-    name: { type: String, require: true },
-    month: { type: Number, min: 1, max: 12, require: true },
-    year: { type: Number, min: 1970, max: 2100, require: true },
+    name: { type: String, required: true },
+    month: { type: Number, min: 1, max: 12, required: true },
+    year: { type: Number, min: 1970, max: 2100, required: true },
     credits: [creditSchema],
     debts: [debtSchema]
 })
